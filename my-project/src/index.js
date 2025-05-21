185f798cd4eb4e7060c830c7a6691f3770fb5ccd
Iwueseiter <preciouschia19@gmail.com>
2025-05-21 17:43:43 +0100

    feat: fix errors

--- my-project/src/index.js
+++ my-project/src/index.js
@@ -1,8 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import './index.css'; 
-=======
->>>>>>> 4eb753c1b99056d527d965594d50c0bbaf57bca7
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
